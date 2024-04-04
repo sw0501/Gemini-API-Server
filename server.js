1eb5cc25fbda6f9f53582c52afbf0869aa2146cf
sw0501 <dkxkqkrtkddn@naver.com>
2024-04-04 09:06:30 +0900

    포트 수정

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = 80;
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 require("dotenv").config();
